a82fd0c2e433817f29d570e5341e6416da5eab43
DashaShevkoplyas <shevkoplyasdarya23@gmail.com>
2020-07-06 18:22:43 +0300

    todo was added

--- src/main/java/implementation/QueriesImpl.java
+++ src/main/java/implementation/QueriesImpl.java
@@ -2,7 +2,8 @@ package implementation;
 
 import entities.Person;
 import interfaces.SqlQueries;
-
+//TODO figure out how to parse values into queries
+// in the way like getName() or about
 public class QueriesImpl implements SqlQueries {
     @Override
     public String selectAll() {
